2cab7725bdda6d41ddb068dd9f5452cbff70315e
kaspersiig <kasp9250@easv365.dk>
2019-11-15 13:01:36 +0100

    Added Job to exports

--- src/job/index.ts
+++ src/job/index.ts
@@ -2,4 +2,5 @@ export * from './job.module';
 export * from './job.controller';
 export * from './job.service';
 export * from './schemas/job.schema';
+export * from './interfaces/job.interface';
 export * from './enums/status.enum';
