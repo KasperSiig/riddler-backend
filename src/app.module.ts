fd05c1b2f8b99313b9d516479984670cb2287335
MissTizz <tizz3n@hotmail.com>
2019-11-15 11:20:05 +0100

    created mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JobModule } from './job';
 import { FileModule } from './file';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [JobModule, FileModule],
+	imports: [
+		JobModule,
+		FileModule,
+		MongooseModule.forRoot(process.env.MONGO_CONN_STRING, {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		}),
+	],
 })
 export class AppModule {}
